31785c222724c0266a1af690344b05343761dbba
AlexUlit <aleksandrulitnikov@gmail.com>
2022-11-20 22:30:24 +0100

    init commit

---
+++ Source/ForTraining/ForTrainingGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ForTrainingGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AForTrainingGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AForTrainingGameMode();
+};
+
+
+
